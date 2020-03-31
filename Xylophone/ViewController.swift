72a5ae7b0bc06a7bd885f9091e9dd0793d1501b9
Wesley Sun <wsun@lcs.on.ca>
2020-03-31 18:39:46 -0400

    Using the sender print to find what color is printing and which button is being pressed

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -7,45 +7,50 @@
 //
 
 import UIKit
-//Audio Visual Foundation
 import AVFoundation
 
-class ViewController: UIViewController {
-    
-    var player: AVAudioPlayer?
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-    }
-
-    @IBAction func keyPressed(_ sender: UIButton) {
+    class ViewController: UIViewController {
         
-    playSound()
-    }
-    
-    func playSound() {
-        guard let url = Bundle.main.url(forResource: "C", withExtension: "wav") else { return }
+        var player: AVAudioPlayer!
 
-        do {
-            try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
-            try AVAudioSession.sharedInstance().setActive(true)
+        override func viewDidLoad() {
+            super.viewDidLoad()
+        }
 
-            /* The following line is required for the player to work on iOS 11. Change the file type accordingly*/
-            player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.mp3.rawValue)
-
-            /* iOS 10 and earlier require the following line:
-            player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileTypeMPEGLayer3) */
-
-            guard let player = player else { return }
-
+        @IBAction func keyPressed(_ sender: UIButton) {
+            playSound()
+            
+            print(sender.backgroundColor)
+        }
+        
+        func playSound() {
+            let url = Bundle.main.url(forResource: "C", withExtension: "wav")
+            player = try! AVAudioPlayer(contentsOf: url!)
             player.play()
-
-        } catch let error {
-            print(error.localizedDescription)
+                    
         }
     }
-    
-    
 
-}
+
+
+
+    
+//    The Online solution code
+//    func playSound() {
+//        guard let url = Bundle.main.url(forResource: "C", withExtension: "wav") else { return }
+//
+//        do {
+//            try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
+//            try AVAudioSession.sharedInstance().setActive(true)
+//
+//            player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.mp3.rawValue)
+//
+//            guard let player = player else { return }
+//
+//            player.play()
+//
+//        } catch let error {
+//            print(error.localizedDescription)
+//        }
+//    }
 
