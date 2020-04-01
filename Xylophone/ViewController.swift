418f450ceb1388c93792072671a034779ed890c7
Wesley Sun <wsun@lcs.on.ca>
2020-03-31 21:58:00 -0400

    Changed the function with the parameters so that every key have a different sound

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -18,13 +18,13 @@ import AVFoundation
         }
 
         @IBAction func keyPressed(_ sender: UIButton) {
-            playSound()
+
+            playSound(soundName:sender.currentTitle! )
             
-            print(sender.backgroundColor)
         }
         
-        func playSound() {
-            let url = Bundle.main.url(forResource: "C", withExtension: "wav")
+        func playSound(soundName: String) {
+            let url = Bundle.main.url(forResource: soundName, withExtension: "wav")
             player = try! AVAudioPlayer(contentsOf: url!)
             player.play()
                     
